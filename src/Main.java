860681eb6aacf4759209292b1bd2366d94320bfb
fishcatairplane <fishcatairplane@gmail.com>
2023-05-07 23:40:55 +0300

    Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import exceprions.WrongAccountException;
+import exceprions.WrongCurrencyException;
+import exceprions.WrongOperationException;
+
 public class Main {
+
     public static void main(String[] args) {
         BankApplication bankApp = new BankApplication();
+        try {
+            bankApp.process("accountId000", 50, "USD");
+        } catch (WrongAccountException e) {
+            System.out.println("Такого акаунту не існує");
+        } catch (WrongCurrencyException e) {
+            System.out.println("Акаунт має рахунок в іншій валюті");
+        } catch (WrongOperationException e) {
+            System.out.println("Акаунт не має достатньо коштів");
+        } catch (Exception e) {
+            System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
+        } finally {
+            System.out.println("Дякуємо, що скористалися нашим сервісом");
+        }
+
+        try {
+            bankApp.process("accountId003", 250, "HRV");
+        } catch (WrongAccountException e) {
+            System.out.println("Такого акаунту не існує");
+        } catch (WrongCurrencyException e) {
+            System.out.println("Акаунт має рахунок в іншій валюті");
+        } catch (WrongOperationException e) {
+            System.out.println("Акаунт не має достатньо коштів");
+        } catch (Exception e) {
+            System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
+        } finally {
+            System.out.println("Дякуємо, що скористалися нашим сервісом");
+        }
+
+        try {
+            bankApp.process("accountId001", 50, "EUR");
+        } catch (WrongAccountException e) {
+            System.out.println("Такого акаунту не існує");
+        } catch (WrongCurrencyException e) {
+            System.out.println("Акаунт має рахунок в іншій валюті");
+        } catch (WrongOperationException e) {
+            System.out.println("Акаунт не має достатньо коштів");
+        } catch (Exception e) {
+            System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
+        } finally {
+            System.out.println("Дякуємо, що скористалися нашим сервісом");
+        }
+
+        try {
+            bankApp.process("accountId001", 50, "USD");
+        } catch (WrongAccountException e) {
+            System.out.println("Такого акаунту не існує");
+        } catch (WrongCurrencyException e) {
+            System.out.println("Акаунт має рахунок в іншій валюті");
+        } catch (WrongOperationException e) {
+            System.out.println("Акаунт не має достатньо коштів");
+        } catch (Exception e) {
+            System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
+        } finally {
+            System.out.println("Дякуємо, що скористалися нашим сервісом");
+        }
+
+        try {
+            bankApp.process("accountId001", 50, "HRV");
+        } catch (WrongAccountException e) {
+            System.out.println("Такого акаунту не існує");
+        } catch (WrongCurrencyException e) {
+            System.out.println("Акаунт має рахунок в іншій валюті");
+        } catch (WrongOperationException e) {
+            System.out.println("Акаунт не має достатньо коштів");
+        } catch (Exception e) {
+            System.out.println("Сталася помилка при процесінгу, спробуйте ще раз");
+        } finally {
+            System.out.println("Дякуємо, що скористалися нашим сервісом");
+        }
     }
 }
